c9bce160e168f3a5d4dd3ca633bc7caa8b2ba039
Zieliheyyy <cleabernadette.payra@gmail.com>
2021-05-08 18:37:10 +0800

    'dashboard new'

--- picme/lib/screens/home/home.dart
+++ picme/lib/screens/home/home.dart
@@ -7,6 +7,8 @@ import 'package:picme/screens/home/lensman_list.dart';
 import 'package:carousel_pro/carousel_pro.dart';
 import 'package:picme/screens/home/client.dart';
 import 'package:google_fonts/google_fonts.dart';
+// import 'package:convex_bottom_bar/convex_bottom_bar.dart';
+
 
 
 
@@ -17,164 +19,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
-  final AuthService _auth = AuthService();
-  TabController tb;
-
-  @override
-  void initState() {
-    tb = TabController(initialIndex: 0, length: 2, vsync: this);
-    super.initState();
-  }
 
   Widget build(BuildContext context) {
-    return StreamProvider<List<Lensman>>.value(
-      value: DatabaseService().lensman,
-      initialData: List(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Dashboard',
-           style: TextStyle(color: Color.fromRGBO(216,181,58,1.0), fontSize: 30.0, fontFamily:'Poppins',fontWeight: FontWeight.bold),),
-          backgroundColor:  Color.fromRGBO(31,31,31,1.0),
-          elevation: 0.0,
-          actions: <Widget>[
-            FlatButton.icon(
-              icon: Icon(Icons.person,
-              color: Color.fromRGBO(216,181,58,1.0),),
-              label: Text('Logout',
-              style: TextStyle(color:Color.fromRGBO(247,247,232,1.0),),
-              ),
-              onPressed: () async {
-                await _auth.signOutUser();
-              },
-            )
-          ],
-        ),
-        body: NestedScrollView(
-          controller: ScrollController(),
-          physics: ClampingScrollPhysics(),
-          headerSliverBuilder: (context, value) {
-            return [
-              SliverAppBar(
-                pinned: true,
-                backgroundColor:  Color.fromRGBO(31,31,31,1.0),
-                flexibleSpace: FlexibleSpaceBar(
-                  background:
-
-            
-                      /// _buildCarousel() in your case....
-            Container(
-                height: 200.0,
-                padding: EdgeInsets.fromLTRB(20, 10, 20, 70),
-               
-              child: ListView(
-                children: <Widget>[
-                  FlatButton( 
-                    padding:
-                     EdgeInsets.fromLTRB(30, 10, 30, 10),
-                    shape: RoundedRectangleBorder(
-                    borderRadius:BorderRadius.circular(8.0)),
-                    color: Color.fromRGBO(237, 237, 237, 1.0),
-                          onPressed: () {
-                               Navigator.of(context).push(MaterialPageRoute(
-                               builder: (context) => Client()));
-                              },
-                    child: Text(
-                        'Profile',
-                          style: TextStyle(
-                         color: Color.fromRGBO(31, 31, 31, 1.0),
-                              fontSize: 20,),
-                  ),
-                ),
-                  SizedBox(
-                    width: double.infinity,  
-                    height: 170.0,  
-                    
-                    child: Carousel(
-                      dotSize: 4.0,
-                      dotBgColor: Colors.transparent,
-                      dotColor: Color.fromRGBO(204,154,72,1.0),
-                      indicatorBgPadding: 5.0,
-                      dotSpacing: 15.0,
-                      dotVerticalPadding: 5.0,
-                    
-
-                      images:[
-                        InkWell(
-                          onTap: (){},
-                          child: Image.asset(
-                            'assets/nct.jpg', 
-                            fit: BoxFit.cover,
-                            
-                          ),
-                        ),
-                        InkWell(
-                          onTap: (){},
-                          child: Image.asset(
-                            'assets/en.jpg', 
-                            fit: BoxFit.cover,
-                            
-                          ),
-                        ),
-                        InkWell(
-                          onTap: (){},
-                          child: Image.asset(
-                            'assets/bts.jpg', 
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ],
-                    ),
-
-                  ),
-                ],
-              ),
-           )
-            ),
-                expandedHeight: 250.0,
-
-                
-                floating: true,
-                bottom: TabBar(
-                  indicatorPadding: EdgeInsets.fromLTRB(20,0,20,0),
-                  unselectedLabelColor:Color.fromRGBO(237,237,237,1.0),
-                  indicator: BoxDecoration(
-                    color: Color.fromRGBO(43,43,43,1.0),
-                    border: Border(
-                    top:  BorderSide(width: 4.0, color: Color.fromRGBO(216,181,58,1.0)),
-                    )
-                  ),
-                  labelColor: Color.fromRGBO(216,181,58,1.0),
-                  indicatorSize: TabBarIndicatorSize.tab,
-                  controller: tb,
-                  tabs: <Widget>[
-                    Tab(
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text("Scheduled",
-                      style: TextStyle(fontFamily: "BarlowBold", fontSize: 15.0)),
-                    ),
-                  ),
-                  Tab(
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text("Urgent",
-                      style: TextStyle(fontFamily: "BarlowBold", fontSize: 15.0)),
-                      
-                    ),
-                  ),
-                  ],
-                ),
-              ),
-            ];
-          },
-          body: TabBarView(
-            controller: tb,
-            children: <Widget>[LensmanList(), LensmanList()],
-          ),
-          
-        ),
-        backgroundColor:  Color.fromRGBO(237,237,237,1.0),
-      ),
-    );
+   
   }
 }
