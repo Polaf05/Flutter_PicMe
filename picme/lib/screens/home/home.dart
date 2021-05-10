338684a4250bf6ffa8a85d29798f2dafe226b6da
Polaf05 <paularpolo1@gmail.com>
2021-05-10 22:46:03 +0800

    bottom tab started

--- picme/lib/screens/home/home.dart
+++ picme/lib/screens/home/home.dart
@@ -12,6 +12,9 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:buttons_tabbar/buttons_tabbar.dart';
 
+import 'client.dart';
+import 'client.dart';
+
 
 class Home extends StatefulWidget {
   @override
@@ -19,31 +22,19 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
-  Widget build(BuildContext context) {
-    final controller = TextEditingController();
-     return StreamProvider<List<Lensman>>.value(
-      value: DatabaseService().lensman,
-      initialData: List(),
-    child: Scaffold(
-      bottomNavigationBar: ConvexAppBar(
-          items: [
-            TabItem(icon: FontAwesomeIcons.home),
-            TabItem(icon: FontAwesomeIcons.userCircle),
-            TabItem(icon: Icons.notifications),
-          ],
-          
-          activeColor: Color.fromRGBO(31, 31, 31, 1.0),
-          backgroundColor: Color.fromRGBO(216, 181, 58, 1.0)),
-      
-      
-      body: Stack(
+
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  final List<Widget> _widgetOptions = <Widget>[
+     Stack(
         children: <Widget>[
            Container(
              height: 52,
               margin: const EdgeInsets.fromLTRB(16, 45, 16, 16),
               padding: const EdgeInsets.symmetric(horizontal: 8),
               child: TextFormField(
-                controller: controller,
+                controller: TextEditingController(),
                    decoration: InputDecoration(
                    border: OutlineInputBorder(
                        borderRadius:
@@ -134,6 +125,142 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
     )
         ],
       ),
+    ClientProfile(),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+     return StreamProvider<List<Lensman>>.value(
+      value: DatabaseService().lensman,
+      initialData: List(),
+    child: Scaffold(
+      bottomNavigationBar: ConvexAppBar(
+          items: [
+            TabItem(icon: FontAwesomeIcons.home),
+            TabItem(icon: FontAwesomeIcons.userCircle),
+            TabItem(icon: Icons.notifications),
+          ],
+          initialActiveIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          
+          activeColor: Color.fromRGBO(31, 31, 31, 1.0),
+          backgroundColor: Color.fromRGBO(216, 181, 58, 1.0)
+          ),
+
+      
+      
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      
+  //     body: Stack(
+  //       children: <Widget>[
+  //          Container(
+  //            height: 52,
+  //             margin: const EdgeInsets.fromLTRB(16, 45, 16, 16),
+  //             padding: const EdgeInsets.symmetric(horizontal: 8),
+  //             child: TextFormField(
+  //               controller: controller,
+  //                  decoration: InputDecoration(
+  //                  border: OutlineInputBorder(
+  //                      borderRadius:
+  //                           BorderRadius.circular(12.0),
+  //                           borderSide: BorderSide(
+  //                           color: Color.fromRGBO(216, 181, 58, 1.0))),
+  //                           hintText: 'Search Lensman',
+  //                           hintStyle: GoogleFonts.raleway(color: Colors.grey, fontSize: 20),
+  //                           prefixIcon: const Icon(Icons.search,
+  //                           color: Color.fromRGBO(216, 181, 58, 1.0),
+  //                             ),
+  //                           ),
+  //                       ),
+  //             ),
+                                           
+         
+  //         Container(
+  //         margin: EdgeInsets.fromLTRB(30,120,30,20),
+  //         width: double.infinity,
+  //         height: 170.0,
+  //         child: Carousel(
+  //           dotSize: 4.0,
+  //           dotBgColor: Colors.transparent,
+  //           dotColor: Color.fromRGBO(204, 154, 72, 1.0),
+  //           indicatorBgPadding: 5.0,
+  //           dotSpacing: 15.0,
+  //           dotVerticalPadding: 5.0,
+  //           images: [
+  //             InkWell(
+  //               onTap: () {},
+  //               child: Image.asset(
+  //                 'assets/nct.jpg',
+  //                 fit: BoxFit.cover,
+  //               ),
+  //             ),
+  //             InkWell(
+  //               onTap: () {},
+  //               child: Image.asset(
+  //                 'assets/en.jpg',
+  //                 fit: BoxFit.cover,
+  //               ),
+  //             ),
+  //             InkWell(
+  //               onTap: () {},
+  //               child: Image.asset(
+  //                 'assets/bts.jpg',
+  //                 fit: BoxFit.cover,
+  //               ),
+  //             ),
+  //           ],
+  //         ),
+  //       ),
+
+  //  Container(
+  //    width: 500,
+  //    margin: EdgeInsets.only(top: 300),
+  //     child: DefaultTabController(
+  //           length: 2,
+  //           child: Column(
+  //             children: <Widget>[
+  //               ButtonsTabBar(
+  //                 buttonMargin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+  //                 contentPadding: EdgeInsets.fromLTRB(40, 0, 45, 0),
+  //                 backgroundColor: Color.fromRGBO(237, 237, 237, 1.0),
+  //                 unselectedBackgroundColor: Color.fromRGBO(216, 181, 58, 1.0),
+  //                 tabs: [
+  //                   Tab(
+  //                     child:Text("Scheduled",
+  //                     style: GoogleFonts.montserrat(fontSize: 15.0, color:Colors.black)),
+  //                   ),
+               
+  //                   Tab(
+  //                     child: Text("Urgent",
+  //                     style: GoogleFonts.montserrat(fontSize: 15.0)),
+  //                    ),
+  //                 ],
+  //               ),
+  //               Expanded(
+  //                 child: TabBarView(
+  //                   children: [
+  //                     LensmanList(), ClientProfile(),
+  //                   ],
+  //                 ),
+  //               ),
+  //             ],
+  //           ),
+  //         ),
+  //   )
+  //       ],
+  //     ),
     ),
     );
   }
