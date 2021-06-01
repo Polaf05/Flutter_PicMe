ddbf83a6b262722d99a8036dd237f5ce3711b706
Polaf05 <paularpolo1@gmail.com>
2021-06-01 12:02:20 +0800

    fixed some codes

--- picme/lib/screens/home/search_data.dart
+++ picme/lib/screens/home/search_data.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:picme/models/lensman.dart';
 import 'package:picme/screens/home/search_tile.dart';
+import 'package:picme/services/database.dart';
 import 'package:provider/provider.dart';
 import 'package:picme/screens/home/lensman_tile.dart';
 
@@ -16,10 +17,12 @@ class SearchData extends StatefulWidget {
 class _SearchDataState extends State<SearchData> {
   @override
   Widget build(BuildContext context) {
+    final DatabaseService _db = DatabaseService();
     return ListView.builder(
         itemCount: widget.snapshotData.docs.length,
         itemBuilder: (BuildContext context, int index) {
-          return SearchTile(lens: widget.snapshotData.docs[index]);
+          dynamic data = _db.lensmanListFromSnapshot(widget.snapshotData);
+          return SearchTile(lens: data[index]);
         });
   }
 }
